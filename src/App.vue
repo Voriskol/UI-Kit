00220f8c39d9f9b2355b34b0d876804bff8f8480
Kirill <pesterevkv2003@gmail.com>
2024-06-12 17:25:41 +0300

    basic template

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import LayoutHeader from "./components/layout/Header.vue";
+import LayoutSideBar from "./components/layout/SideBar.vue";
+import { ref } from "vue";
+
+const isOpenMenu = ref(false);
+
+function toggleMenu() {
+  isOpenMenu.value = !isOpenMenu.value;
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="sidebar-toggle" @click="toggleMenu">&#5125;</div>
+    <LayoutHeader />
+    <LayoutSideBar :openSideBar="isOpenMenu" />
+    <div :class="['content', { content_full: !isOpenMenu }]">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+@import './styles/global.sass'
+
+.content
+  max-width: 1400px
+  margin-left: 250px
+  padding: 30px
+  transition: .2s
+  &_full
+    margin-left: 0
+
+
+.sidebar-toggle
+  position: fixed
+  left: 0
+  width: 15px
+  background: var(--primary-color)
+  height: 100%
+  top: 62px
+  z-index: 1
+  cursor: pointer
+  color: #fff
+  display: flex
+  align-items: center
+  justify-content: center
+
+
+@media screen and (max-width: 1023px)
+  .content
+    margin-left: 0
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="heading-1">Welcome to our UI Kit</h1>
+</template>
